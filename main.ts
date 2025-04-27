306537153b104b4d018ca73de80aea8f23e1b5ed
Sune Sylvest Nilausen <sunenilausen@gmail.com>
2025-04-27 13:19:51 +0200

    simplified some vars to danish var names

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ namespace SpriteKind {
 }
 sprites.onOverlap(SpriteKind.Projectile2, SpriteKind.Player, function (sprite, otherSprite) {
     mp.changePlayerStateBy(mp.playerSelector(mp.PlayerNumber.Two), MultiplayerState.score, 1)
-    sprites.destroy(sprite, effects.fire, 100)
+    sprites.destroy(sprite, effects.fountain, 100)
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player2, function (sprite, otherSprite) {
     mp.changePlayerStateBy(mp.playerSelector(mp.PlayerNumber.One), MultiplayerState.score, 1)
@@ -12,22 +12,39 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player2, function (sprite, o
 })
 mp.onButtonEvent(mp.MultiplayerButton.A, ControllerButtonEvent.Pressed, function (player2) {
     if (mp.isButtonPressed(mp.playerSelector(mp.PlayerNumber.One), mp.MultiplayerButton.A)) {
-        player2_projectile = sprites.create(sprites.projectile.explosion1, SpriteKind.Projectile)
-        player2_projectile.setPosition(player_a.x, player_a.y)
-        player2_projectile.setVelocity(player_a.vx, player_a.vy)
+        skud = sprites.create(sprites.projectile.explosion1, SpriteKind.Projectile)
+        skud.setPosition(heks.x, heks.y)
+        skud.setVelocity(heks.vx, heks.vy)
     } else if (mp.isButtonPressed(mp.playerSelector(mp.PlayerNumber.Two), mp.MultiplayerButton.A)) {
-        player2_projectile = sprites.create(sprites.projectile.explosion1, SpriteKind.Projectile2)
-        player2_projectile.setPosition(player_b.x, player_b.y)
-        player2_projectile.setVelocity(player_b.vx, player_b.vy)
+        skud = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 6 6 . . . . . . . 
+            . . . . . . 6 9 9 6 . . . . . . 
+            . . . . . . 8 9 9 8 . . . . . . 
+            . . . . . . . 8 8 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Projectile2)
+        skud.setPosition(fe.x, fe.y)
+        skud.setVelocity(fe.vx, fe.vy)
     }
 })
-let player2_projectile: Sprite = null
-let player_b: Sprite = null
-let player_a: Sprite = null
-player_a = sprites.create(sprites.swamp.witchBack0, SpriteKind.Player)
-player_b = sprites.create(sprites.castle.princess2Back, SpriteKind.Player2)
-mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), player_a)
-mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), player_b)
+let skud: Sprite = null
+let fe: Sprite = null
+let heks: Sprite = null
+heks = sprites.create(sprites.swamp.witchBack0, SpriteKind.Player)
+fe = sprites.create(sprites.castle.princess2Back, SpriteKind.Player2)
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), heks)
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), fe)
 mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One), 100, 100)
 mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Two), 100, 100)
 mp.setPlayerState(mp.playerSelector(mp.PlayerNumber.One), MultiplayerState.score, 0)
